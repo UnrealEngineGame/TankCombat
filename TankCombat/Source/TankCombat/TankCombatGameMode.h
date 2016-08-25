1a4a03a9ab8684b297d9db3da8f064b5055e87d0
KOMPOZER3-PC\dinod <dino.duvnjak24@gmail.com>
2016-08-25 17:10:27 +0200

    Coloring landscape finish

---
+++ TankCombat/Source/TankCombat/TankCombatGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankCombatGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKCOMBAT_API ATankCombatGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
