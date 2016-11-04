6eee1c5e5129f65aebd519fd60adadf63d5fd62a
KOMPOZER3-PC\dinod <dino.duvnjak24@gmail.com>
2016-11-04 14:36:24 +0100

    Fucking tank rotation unlocked

--- TankCombat/Source/TankCombat/Private/TankTrack.cpp
+++ TankCombat/Source/TankCombat/Private/TankTrack.cpp
@@ -9,11 +9,16 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("Direction: %s, Throttle: %f"), *GetName(), Throttle);
 
 	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	UE_LOG(LogTemp, Warning, TEXT("GetForwardVector: %s"), *GetForwardVector().ToString());
+
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	UE_LOG(LogTemp, Warning, TEXT("Root: %s"), *GetOwner()->GetName());
+
 	if (!TankRoot) { return; }
 	UE_LOG(LogTemp, Error, TEXT("ForceApplied : %s"), *ForceApplied.ToString());
 
 	TankRoot->AddForceAtLocation(ForceApplied, GetComponentLocation());
+
 }
 
 
